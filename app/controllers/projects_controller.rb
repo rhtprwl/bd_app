85cc0355ce913b3e0f83e847976771457ae529c3
monikasonia <monika.1993.sonia@gmail.com>
2014-03-24 16:41:04 +0530

    updation and deletion in database table, layout and designing work

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,13 +13,35 @@ class ProjectsController < ApplicationController
   	end  
   end
 
-  def show
+  def index
   	@project=Project.all
   end	
+
+  def edit
+   @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+   
+    if @project.update(project_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project=Project.find(params[:id])
+    @project.destroy
+
+    redirect_to project_path
+  end
 
    private
 
     def project_params
       params.require(:project).permit(:PROJECT, :URL, :DOMAIN, :TECHNOLOGY, :TAGS, :START_DATE, :END_DATE, :PROJECT_LEAD, :PROJECT_TYPE, :DESCRIPTION)
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 BdApp::Application.routes.draw do
   devise_for :users
 
-  resources :projects, only: [:new, :create, :show]
+  resources :projects
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-   root to: "projects#show"
+   root to: "projects#index"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
