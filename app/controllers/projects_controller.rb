af91dfa612b129c017d0602efa6354285b172c1f
dilip240 <diliprathore240@gmail.com>
2014-03-25 14:24:20 +0530

    edit delete in proper working form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -15,6 +15,27 @@ class ProjectsController < ApplicationController
   	end
   end
 
+    def edit
+
+      @project = Project.find(params[:id])
+    end
+
+    def update
+
+     @project = Project.find(params[:id])
+      if @project.update(project_params)
+       redirect_to root_path
+      else
+        render 'edit'
+      end
+    end
+   
+   def destroy
+     @project=Project.find(params[:id])
+     @project.destroy
+     redirect_to project_path
+   end
+
    def index
          #logger.debug "aaaaaaaaaaaaaaaaaaaa"
         if params[:search]
